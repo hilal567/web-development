019df330cfee14ad9efbad1eb394631c18ef37cc
ronbrian <ronmachoka@gmail.com>
2018-07-06 16:09:03 +0300

    ..
    ..

--- 2nd/pages/Admin/promotions.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("header.php");
-/*IF SESSION IS SET....SHOW THE NAVBAR*/
-require_once("navbar.php");
-?>
---
+++ project2/pages/Users/home.php
@@ -0,0 +1,29 @@
+<?php
+require_once('header.php');
+?>
+
+
+
+
+
+<!-- Page Content -->
+<div style="margin-left:18%">
+    
+<div class="row">
+    <div class="leftcolumn">
+
+
+
+    </div>
+
+
+
+
+</div>
+      
+</body>
+</html>
+
+
+
+
